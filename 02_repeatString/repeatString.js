0ad80bc29b42ce161b2c15811664a857a782f10b
mcmullarkey <mullarkey.mike@gmail.com>
2024-09-06 16:25:34 -0600

    Exercise complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+
+    if (num < 0) {
+        return "ERROR"
+    } 
+
+    let result = "";
+
+    for (let i = 0; i < num; i++) {
+        result += str;  // Concatenates str each time the loop runs
+      }
+      
+      return result;
 
 };
 
